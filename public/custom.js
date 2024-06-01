f818fb2f4706a7eba68baa7d1bb13a38e99155ae
Pyae Sone Aung <pyaesoneaung.code@gmail.com>
2024-06-01 15:24:36 +0630

    wait: complete state

--- public/custom.js
+++ public/custom.js
@@ -1,17 +1,18 @@
-docsearch({
-    appId: 'SQHZQLFJMP',
-    apiKey: '4c28157f280bcebf4cd50e7a7cf0d4bd',
-    indexName: 'pyaesoneaung',
-    container: 'div#docSearch',
-});
-
-document.getElementById('searchIcon')
-    .addEventListener(
-        'click',
-        clickDocSearch
-    );
-
-function clickDocSearch() {
-    const button = document.getElementsByClassName('DocSearch-Button');
-    button[0].click();
-}
+document.onreadystatechange = () => {
+    if (document.readyState === 'complete') {
+        docsearch({
+            appId: 'SQHZQLFJMP',
+            apiKey: '4c28157f280bcebf4cd50e7a7cf0d4bd',
+            indexName: 'pyaesoneaung',
+            container: 'div#docSearch',
+        });
+    
+        document.getElementById('searchIcon')
+            .addEventListener('click', clickDocSearch);
+    
+        function clickDocSearch() {
+            const button = document.getElementsByClassName('DocSearch-Button');
+            button[0].click();
+        }
+    }
+};
